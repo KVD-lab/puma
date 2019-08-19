382535255aaa6eb52defb90385dba66c831aa60a
Ken Youens-Clark <kyclark@gmail.com>
2019-08-19 11:57:34 -0700

    changes for 1.0

--- stampede/run.sh
+++ stampede/run.sh
@@ -4,18 +4,19 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -p normal
-#SBATCH -t 24:00:00
+#SBATCH -t 12:00:00
 #SBATCH -A iPlant-Collabs
 
 module load tacc-singularity
 
-IMG="/work/05066/imicrobe/singularity/puma-0.1.0.img"
+IMG="/work/05066/imicrobe/singularity/puma-1.0.0.img"
 
 if [[ ! -e "$IMG" ]]; then
     echo "Missing Singularity image \"$IMG\""
     exit 1
 fi
 
-singularity exec $IMG puma.py "$@" -o puma-out
+DATA_DIR="/work/05066/imicrobe/iplantc.org/data/puma/data_dir"
+singularity exec $IMG run_puma.py "$@" -o puma_out -d ${DATA_DIR} -D info
 
 echo "Comments to kyclark@email.arizona.edu"
